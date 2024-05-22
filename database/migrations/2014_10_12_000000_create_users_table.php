ff455f4f2a8fff385c05666a5e46e8d7f8389341
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-05-22 14:59:39 +0700

    revisi seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id_user')->unique();
+            $table->id();
             $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
