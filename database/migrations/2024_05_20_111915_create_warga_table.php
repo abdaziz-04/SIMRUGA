d93b9a382cf091fc15facc07d72d34cd62666894
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-05-24 03:02:52 +0700

    perbaiki resource warga

--- database/migrations/2024_05_20_111915_create_warga_table.php
+++ database/migrations/2024_05_20_111915_create_warga_table.php
@@ -1,50 +1,34 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+namespace App\Models;
 
-return new class extends Migration
+use Illuminate\Database\Eloquent\Model;
+
+class Warga extends Model
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('warga', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_warga', 255);
-            $table->string('alamat', 255);
-            $table->string('no_telepon', 25);
-            $table->string('email', 25);
-            $table->string('NIK', 25);
-            $table->date('tanggal_lahir');
-            $table->char('jenis_kelamin', 1);
-            $table->string('status_kawin', 25);
-            $table->string('pekerjaan', 255);
-            $table->string('foto_warga')->nullable();
-            $table->string('transportasi');
-            $table->string('status_kepemilikan_rumah');
-            $table->string('status_perkawinan');
-            $table->string('sumber_air_bersih');
-            $table->string('penerangan_rumah');
-            $table->string('luas_bangunan');
-            $table->decimal('pengeluaran_bulanan', 15, 2);
-            $table->string('jumlah_anggota_keluarga');
-            $table->string('penghasilan');
-            $table->string('tanggungan');
-            $table->string('jenis_warga');
-            $table->timestamps();
-        });
-    }
+    protected $table = 'warga';
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('warga');
-    }
-};
+    protected $fillable = [
+        'nama_warga',
+        'alamat',
+        'no_telepon',
+        'email',
+        'NIK',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'status_kawin',
+        'pekerjaan',
+        'foto_warga',
+        'transportasi',
+        'status_kepemilikan_rumah',
+        'status_perkawinan',
+        'sumber_air_bersih',
+        'penerangan_rumah',
+        'luas_bangunan',
+        'pengeluaran_bulanan',
+        'jumlah_anggota_keluarga',
+        'penghasilan',
+        'tanggungan',
+        'jenis_warga',
+    ];
+}
