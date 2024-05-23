7ffa3e76da504b5251157dcb25330bfadfcdd429
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-05-23 22:49:34 +0700

    buat login dengan username

---
+++ app/Filament/Auth/CustomLogin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Auth;
+use Filament\Pages\Auth\Login;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
+
+class CustomLogin extends Login
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getLoginFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label(__('Username'))
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        return [
+            'username' => $data['login'],
+            'password' => $data['password'],
+        ];
+    }
+}
