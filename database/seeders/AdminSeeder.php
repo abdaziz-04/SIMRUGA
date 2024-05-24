d2447b6009bc73e5827ac2037af94ec423ce6e89
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-05-24 15:26:27 +0700

    seeder permission untuk roles

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,8 +15,14 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['username' => 'admin', 'name'=>'Admin', 'password' => Hash::make('12345'),],
+            ['username' => 'admin', 'name'=>'Admin', 'password' => Hash::make('12345admin'),],
+            ['username' => 'sekretaris', 'name' => 'Sekretaris', 'password' => Hash::make('12345')],
+            ['username' => 'bendahara', 'name' => 'Bendahara', 'password' => Hash::make('12345')],
+            ['username' => 'ketua_rw', 'name' => 'Ketua RW', 'password' => Hash::make('12345')],
+            ['username' => 'ketua_rt', 'name' => 'Ketua RT', 'password' => Hash::make('12345')],
+            ['username' => 'warga', 'name' => 'Warga', 'password' => Hash::make('12345')],
         ];
+    
     
         DB::table('users')->insert($data);
     }
