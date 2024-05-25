dde8e0084b7ce3fb9c0c7b891e52263631113d1b
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-05-25 16:13:12 +0700

    revisi warga model

--- app/Models/Warga.php
+++ app/Models/Warga.php
@@ -6,28 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Warga extends Model
 {
-      protected $table = 'warga';
-      protected $primaryKey = 'id';
-      protected $fillable = [
-          'nama_warga',
-          'alamat',
-          'no_telepon',
-          'email',
-          'NIK',
-          'tanggal_lahir',
-          'jenis_kelamin',
-          'status_kawin',
-          'pekerjaan',
-          'foto_warga',
-          'transportasi',
-          'status_kepemilikan_rumah',
-          'status_perkawinan',
-          'sumber_air_bersih',
-          'penerangan_rumah',
-          'luas_bangunan',
-          'pengeluaran_bulanan',
-          'jumlah_anggota_keluarga',
-          'penghasilan',
-          'tanggungan',
-          'jenis_warga',
+    protected $table = 'warga';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nama_warga',
+        'alamat',
+        'no_telepon',
+        'email',
+        'NIK',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'status_kawin',
+        'pekerjaan',
+        'foto_warga',
+        'transportasi',
+        'status_kepemilikan_rumah',
+        'status_perkawinan',
+        'sumber_air_bersih',
+        'penerangan_rumah',
+        'luas_bangunan',
+        'pengeluaran_bulanan',
+        'jumlah_anggota_keluarga',
+        'penghasilan',
+        'tanggungan',
+        'jenis_warga',
+    ];
 }
