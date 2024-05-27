5ccc6cc264b1d75d77a922ff9456fadd977733da
abdaziz-04 <abdaziz1713.com>
2024-05-27 22:06:49 +0700

    auth

--- app/Filament/Resources/RTResource.php
+++ app/Filament/Resources/RTResource.php
@@ -29,6 +29,14 @@ class RTResource extends Resource
         return static::getModel()::count();
     }
 
+    public static function shouldRegisterNavigation(): bool // Sembunyiin dari navigasi
+    {
+        if(auth()->user()->can('view_daftar_rt')) // string dalem can sesuain sama permission yang dibuat
+            return true;
+        else
+            return false;
+    }
+
     public static function form(Form $form): Form
     {
         return $form
