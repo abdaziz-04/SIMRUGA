35a584b6817dce0e4ccc37ff7bd17c9d6e3909a4
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-05-27 11:28:54 +0700

    perbaikan konflik

--- database/seeders/KKSeeder.php
+++ database/seeders/KKSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
-use App\Models\KK;
-use App\Models\Warga;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class KKSeeder extends Seeder
 {
@@ -15,16 +13,25 @@ class KKSeeder extends Seeder
      */
     public function run(): void
     {
-        // Ambil contoh warga yang sudah ada
-        $warga = Warga::first();
-
-        if ($warga) {
-            KK::create([
+        DB::table('kartu_keluarga')->insert([
+            [
                 'nama_kepala_keluarga' => 'John Doe',
                 'no_kk' => '1234567890123456',
-                'rt_rw' => '001/002',
-                'alamat' => 'Jl. Kebon Jeruk No. 10',
-            ]);
-        }
+                'rt_rw' => '01/02',
+                'alamat' => 'Jl. Example No. 1, Jakarta',
+            ],
+            [
+                'nama_kepala_keluarga' => 'Jane Smith',
+                'no_kk' => '1234567890123457',
+                'rt_rw' => '02/03',
+                'alamat' => 'Jl. Example No. 2, Jakarta',
+            ],
+            [
+                'nama_kepala_keluarga' => 'Robert Brown',
+                'no_kk' => '1234567890123458',
+                'rt_rw' => '03/04',
+                'alamat' => 'Jl. Example No. 3, Jakarta',
+            ],
+        ]);
     }
 }
