5f44f605de3b4bab4bdb4b47c4953f0ef413eee1
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-06-03 15:25:54 +0700

    seeder dan migration warga

--- database/migrations/2014_05_23_201309_create_table_warga.php
+++ database/migrations/2014_05_23_201309_create_table_warga.php
@@ -24,8 +24,8 @@ return new class extends Migration
             $table->string('status_kawin', 25);
             $table->string('pekerjaan', 255);
             $table->string('foto_warga')->nullable();
-            $table->string('status_kepemilikan_rumah');
-            $table->string('sumber_air_bersih');
+            $table->integer('status_kepemilikan_rumah');
+            $table->integer('sumber_air_bersih');
             $table->integer('token_listrik');
             $table->integer('jumlah_anggota_keluarga');
             $table->integer('penghasilan');
