42b41cbce99cf52f0581f21d0064a799a1b56fba
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-06-03 17:04:29 +0700

    nullable 2

--- database/migrations/2014_05_23_201309_create_table_warga.php
+++ database/migrations/2014_05_23_201309_create_table_warga.php
@@ -13,24 +13,24 @@ return new class extends Migration
     {
         Schema::create('warga', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('id_kk')->constrained('kartu_keluarga')->onDelete('cascade')->nullable();
-            $table->foreignId('id_rt')->constrained('rt')->nullable();
-            $table->string('nama_warga', 255)->nullable();
-            $table->string('alamat', 255)->nullable();
-            $table->string('no_telepon', 25)->nullable();
-            $table->string('NIK', 25)->nullable();
-            $table->date('tanggal_lahir')->nullable();
-            $table->char('jenis_kelamin', 1)->nullable();
+            $table->foreignId('id_kk')->constrained('kartu_keluarga')->onDelete('cascade');
+            $table->foreignId('id_rt')->constrained('rt');
+            $table->string('nama_warga', 255);
+            $table->string('alamat', 255);
+            $table->string('no_telepon', 25);
+            $table->string('NIK', 25);
+            $table->date('tanggal_lahir');
+            $table->char('jenis_kelamin', 1);
             $table->string('status_kawin', 25)->nullable();
             $table->string('pekerjaan', 255)->nullable();
-            $table->string('foto_warga')->nullable()->nullable();
+            $table->string('foto_warga')->nullable();
             $table->integer('status_kepemilikan_rumah')->nullable();
             $table->integer('sumber_air_bersih')->nullable();
             $table->integer('token_listrik')->nullable();
             $table->integer('umur')->nullable();
             $table->integer('penghasilan')->nullable();
             $table->integer('tanggungan')->nullable();
-            $table->string('jenis_warga')->nullable();
+            $table->string('jenis_warga');
             $table->timestamps();
         });
     }
