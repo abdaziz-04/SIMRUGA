bf322b0f0d756bafeacac574b9189e335b9434c5
abdaziz-04 <abdaziz1713.com>
2024-06-07 00:31:29 +0700

    custom edit profile

--- resources/views/vendor/filament-edit-profile/filament/pages/edit-profile-page.blade.php
+++ resources/views/vendor/filament-edit-profile/filament/pages/edit-profile-page.blade.php
@@ -1,34 +1,20 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="updateProfile">
-        {{ $this->editProfileForm }}
-
-        <x-filament-panels::form.actions
-            alignment="right"
-            :actions="$this->getUpdateProfileFormActions()"
-        />
-    </x-filament-panels::form>
 
     <x-filament-panels::form wire:submit="updatePassword">
         {{ $this->editPasswordForm }}
 
-        <x-filament-panels::form.actions
-            alignment="right"
-            :actions="$this->getUpdatePasswordFormActions()"
-        />
+        <x-filament-panels::form.actions alignment="right" :actions="$this->getUpdatePasswordFormActions()" />
     </x-filament-panels::form>
 
-    @if(config('filament-edit-profile.show_custom_fields'))
+    @if (config('filament-edit-profile.show_custom_fields'))
         <x-filament-panels::form wire:submit="updateCustomFields">
             {{ $this->customFieldsForm }}
 
-            <x-filament-panels::form.actions
-                alignment="right"
-                :actions="$this->getUpdateCustomFieldsFormActions()"
-            />
+            <x-filament-panels::form.actions alignment="right" :actions="$this->getUpdateCustomFieldsFormActions()" />
         </x-filament-panels::form>
     @endif
 
-    @if($this->shouldShowSanctumTokens())
+    @if ($this->shouldShowSanctumTokens())
         <x-filament::section aside>
             <x-slot name="heading">
                 {{ __('filament-edit-profile::default.token_section_title') }}
@@ -41,13 +27,13 @@
         </x-filament::section>
     @endif
 
-    @if($this->shouldShowBrowserSessionsForm())
+    @if ($this->shouldShowBrowserSessionsForm())
         <x-filament-panels::form>
             {{ $this->browserSessionsForm }}
         </x-filament-panels::form>
     @endif
 
-    @if($this->shouldShowDeleteAccountForm())
+    @if ($this->shouldShowDeleteAccountForm())
         <x-filament-panels::form>
             {{ $this->deleteAccountForm }}
         </x-filament-panels::form>
