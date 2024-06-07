5b0107a306367146fa4aca3e3d754f25b91222f6
abdaziz-04 <abdaziz1713.com>
2024-06-08 00:56:38 +0700

    perbaikan view lembaga pendukung

--- app/Filament/Resources/LembagaPendukungResource.php
+++ app/Filament/Resources/LembagaPendukungResource.php
@@ -2,25 +2,36 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\LembagaPendukungResource\Pages;
-use App\Filament\Resources\LembagaPendukungResource\RelationManagers;
-use App\Models\LembagaPendukung;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Actions\DeleteAction;
+use App\Models\LembagaPendukung;
+use Filament\Infolists\Infolist;
+use Filament\Infolists;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Select;
+use Filament\Infolists\Components\Grid;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Card;
+use Filament\Infolists\Components\Group;
+use Filament\Infolists\Components\Split;
 use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Actions\DeleteAction;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\LembagaPendukungResource\Pages;
+use App\Filament\Resources\LembagaPendukungResource\RelationManagers;
+
 
 class LembagaPendukungResource extends Resource
 {
@@ -54,7 +65,6 @@ class LembagaPendukungResource extends Resource
             ->columns([
                 TextColumn::make('nama_lembaga')->label('Nama Lembaga'),
                 TextColumn::make('kontak_lembaga')->label('No Telpon'),
-                TextColumn::make('deskripsi_lembaga')->label('Deskripsi'),
             ])
             ->filters([
                 //
@@ -62,13 +72,31 @@ class LembagaPendukungResource extends Resource
             ->actions([
                 Action::make('lapor')
                     ->label('Lapor')
+                    ->icon('heroicon-o-document')
                     ->url(fn (LembagaPendukung $record): string => $record->whatsappLink())
-                    ->openUrlInNewTab(), // Membuka tautan di tab baru
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                    ->openUrlInNewTab(),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make()
+                    ->schema([
+                        Split::make([
+                            Grid::make(2)
+                                ->schema([
+                                    Group::make([
+                                        TextEntry::make('nama_lembaga'),
+                                        TextEntry::make('kontak_lembaga'),
+                                    ]),
+                                    Group::make([
+                                        TextEntry::make('deskripsi_lembaga'),
+                                    ]),
+                                ]),
+                        ])->from('lg'),
+                    ]),
             ]);
     }
 
@@ -84,6 +112,7 @@ class LembagaPendukungResource extends Resource
         return [
             'index' => Pages\ListLembagaPendukungs::route('/'),
             'create' => Pages\CreateLembagaPendukung::route('/create'),
+            'view' => Pages\ViewLembagaPendukung::route('/{record}'),
             'edit' => Pages\EditLembagaPendukung::route('/{record}/edit'),
         ];
     }
