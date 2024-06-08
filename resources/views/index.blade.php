652008910f9ac0106f314b5be005bc5ac4f4ade9
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-06-08 13:44:48 +0700

    LandingPage done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -62,16 +62,17 @@
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="text-start">
+                <div class="text-start"data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
                     <h1 class="display-2 fw-bolder">RW 8</h1>
                     <h2 class="display-5 fw-bolder">Sawojajar</h2>
                     <h3 class="display-5 fw-bolder">Kota Malang</h3>
-                    <p style="line-height: 1.5; margin-bottom: 30px;">
-                        SIMRUGA / Sistem Informasi Rukun Warga merupakan pusat informasi bagi RW 8
-                        Kelurahan Sawojajar Kecamatan Kedungkandang Kota Malang.
+                    <p style="line-height: 1.5; margin-bottom: 30px; font-size: 20px">
+                        Temukan kemudahan dalam mengrus adminitrasi dan data pribadimu bersama SIMRUGA
                     </p>
+                </div>
+                <div data-aos="zoom-in-right">
                     <a class="btn btn-primary text-uppercase" role="button" href="/admin/login">
-                        Simruga
+                        cek lebih lanjut
                     </a>
                 </div>
             </div>
@@ -82,10 +83,13 @@
         <section id="services">
             <div class="container">
                 <div class="row text-left">
-                    @foreach ($data_pengumuman as $pengumuman)
-                        <div class="col-md-3 mb-4">
+                    @foreach ($data_pengumuman as $index => $pengumuman)
+                        <div class="col-md-3 mb-4" data-aos="fade-down" data-aos-delay="{{ $index * 100 }}">
                             <div class="card h-100">
-                                <img src="{{ $pengumuman['gambar'] }}" class="card-img-top" alt="Gambar Pengumuman">
+                                <div class="card-img-top d-flex align-items-center justify-content-center"
+                                    style="height: 200px; background-color: #eaeaea;">
+                                    <span class="fw-bolder" style="font-size: 24px; color: #333;">simruga</span>
+                                </div>
                                 <div class="card-body d-flex flex-column">
                                     <h5 class="card-title">{{ $pengumuman['nama_pengumuman'] }}</h5>
                                     <a href="{{ route('pengumuman', $pengumuman->id_pengumuman) }}"
@@ -101,25 +105,35 @@
 
     <div class="container mt-0">
         <div class="col-lg-12 text-center">
-            <div class="col-lg-12 text-center">
+            <div class="col-lg-12 text-center" data-aos="fade-down">
                 <h2 class="display-5 fw-bolder">Dalam Grafik</h2>
+                <p style="line-height: 1.5; margin-bottom: 30px; font-size: 20px">
+                    Menunjukan penyajian data warga dalam bentuk grafis
+                </p>
             </div>
+            <div data-aos="fade-up">
             <iframe class="embed-responsive-item"
                 src="https://lookerstudio.google.com/embed/reporting/d5d1ecd9-9d72-47e6-8eb7-a2e443e4b460/page/p_vfix41czhd"
                 width="100%" height="1015" style="border:0;" allowfullscreen="" loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
     </div>
 
     <section id="services">
         <div class="container mt-0">
 
-            <div class="col-lg-12 text-center">
-                <h2 class="display-5 fw-bolder">Fitur SIMRUGA</h2>
+            <div class="col-lg-12 text-center" data-aos="zoom-in-up">
+                <h2 class="display-5 fw-bolder">Tentang SIMRUGA</h2>
+                <p style="line-height: 1.5; margin-bottom: 30px; font-size: 20px">
+                    SIMRUGA / Sistem Informasi Rukun Warga merupakan pusat informasi bagi RW 8
+                    yang digitalisasikan agar memungkinkan mempermudah mobilitas informasi, data,
+                    dan proses adminitrasi warga.
+                </p>
             </div>
 
             <div class="row text-center">
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-down" >
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-blue"></i>
                         <i class="fa fa-newspaper-o fa-stack-1x fa-inverse"></i>
@@ -128,7 +142,7 @@
                     <p class="text-muted">Menyajikan berita terupdate seputar RW 8 Kelurahan Sawojajar Kecamatan
                         Kedungkandang</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-down" >
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-blue"></i>
                         <i class="fa fa-paste fa-stack-1x fa-inverse"></i>
@@ -139,8 +153,8 @@
                         di dalam kondisi apapun</p>
                 </div>
             </div>
-            <div class="row text-center">
-                <div class="col-md-6">
+            <div class="row text-center"  >
+                <div class="col-md-6" data-aos="fade-down">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-blue"></i>
                         <i class="fa fa-money fa-stack-1x fa-inverse"></i>
@@ -149,7 +163,7 @@
                     <p class="text-muted">Menampilkan pelaporan keuangan RW mulai dari uang masuk hingga uang keluar
                         secara real time</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6" data-aos="fade-down">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-blue"></i>
                         <i class="fa fa-database fa-stack-1x fa-inverse"></i>
@@ -165,10 +179,10 @@
     <section id="map">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
+                <div class="col-lg-12 text-center" data-aos="fade-down">
                     <h2 class="display-5 fw-bolder">Lokasi RW 8</h2>
                 </div>
-                <div class="col-lg-12 text-center">
+                <div class="col-lg-12 text-center" data-aos="fade-up">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.1959812314476!2d112.65307167368537!3d-7.978684992046556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd62842c935c8f1%3A0x72cecd5a022b06fa!2sJl.%20Simpang%20Danau%20Maninjau%20Sel.%20Dalam%20I%2C%20Sawojajar%2C%20Kec.%20Kedungkandang%2C%20Kota%20Malang%2C%20Jawa%20Timur%2065139!5e0!3m2!1sid!2sid!4v1717262417808!5m2!1sid!2sid"
                         width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
@@ -181,23 +195,24 @@
 
     <section id="team">
         <div class="container">
-            <div class="col-lg-12 text-center">
+            <div class="col-lg-12 text-center" data-aos="zoom-in-up">
                 <h2 class="display-5 fw-bolder">Hubungi Pengurus RW</h2>
             </div>
             <div class="row">
-                @foreach ($pengurus as $member)
-                    <div class="col-lg-3 col-md-4 col-sm-6 mb-0">
+                @foreach ($pengurus as $index => $member)
+                    <div class="col-lg-3 col-md-4 col-sm-6 mb-0" data-aos="fade-down" data-aos-delay="{{ $index * 100 }}">
                         <div class="team-member">
                             {{-- <img class="rounded-circle mx-auto" src="assets/img/team/{{ $member->nama_pengurus }}.jpg"
                                 alt="{{ $member->nama_pengurus }}"> --}}
                             <h4>{{ $member->nama_pengurus }}</h4>
                             <p class="text-muted">{{ $member->jabatan }}</p>
                             <ul class="list-inline social-buttons">
-                                <li class="list-inline-item"><a
-                                        href="https://wa.me/{{ $member->no_telepon }}?text=Halo,%20saya%20tertarik%20untuk%20menghubungi%20Anda."
-                                        target="_blank">
-                                        <i class="fa fa-whatsapp"></i></li>
-                                </a>
+                                <li class="list-inline-item">
+                                    <a href="https://wa.me/{{ $member->no_telepon }}?text=Halo,%20saya%20tertarik%20untuk%20menghubungi%20Anda."
+                                       target="_blank">
+                                        <i class="fa fa-whatsapp"></i>
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -229,7 +244,7 @@
             </div>
         </div>
     </footer>
-    
+
 
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
