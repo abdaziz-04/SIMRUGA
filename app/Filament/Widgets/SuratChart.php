976697ef638047b4b15146d1bb9022af01594734
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-06-11 05:01:45 +0700

    widget surat done

---
+++ app/Filament/Widgets/SuratChart.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\SuratSekretaris;  
+use App\Models\SuratWarga;
+use Illuminate\Support\Facades\Auth;
+
+class SuratChart extends ChartWidget
+{
+protected static ?string $heading = 'Surat';
+
+protected function getData(): array
+{
+
+
+        //Menghitung jumlah surat masuk dari surat_sekretaris
+        $jumlahSuratMasuk = SuratSekretaris::count();
+        
+        //Menghitung jumlah surat keluar dari surat_Warga
+        $jumlahSuratKeluar = SuratWarga::count();
+
+        return [
+            'labels' => ['Surat Masuk', 'Surat Keluar'],
+            'datasets' => [
+                [
+                    'label' => 'Jumlah Surat',
+                    // 'backgroundColor' => ['#4CAF50', '#FF6384'],
+                    'data' => [$jumlahSuratMasuk, $jumlahSuratKeluar],
+                ],
+            ],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+
+    
+}
