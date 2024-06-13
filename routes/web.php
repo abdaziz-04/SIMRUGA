032bb9a933c95ec1bf15c80d324383b4da93bafc
abdaziz-04 <abdaziz1713.com>
2024-06-13 10:58:19 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PengumumanController;
 use App\Http\Controllers\SuratKematianController;
 use App\Http\Controllers\PerhitunganBansosController;
 use App\Filament\Resources\SuratResource\Pages\ViewSurat;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,33 @@ use App\Filament\Resources\SuratResource\Pages\ViewSurat;
 Route::get('/', [LandingController::class, 'index']);
 // Route::get('/',[PengumumanController::class,'index']);
 Route::get('/pengumuman/{id}', [PengumumanController::class, 'show'])->name('pengumuman');
+
+Route::get('/migrate-fresh', function () {
+    Artisan::call('migrate:fresh');
+    return 'Database migrated fresh!';
+})->middleware('auth', 'admin');
+
+Route::get('/db-seed', function () {
+    Artisan::call('db:seed');
+    return 'Database seeded!';
+})->middleware('auth', 'admin');
+
+Route::get('/cache-clear', function () {
+    Artisan::call('cache:clear');
+    return 'Cache cleared!';
+})->middleware('auth', 'admin');
+
+Route::get('/config-clear', function () {
+    Artisan::call('config:clear');
+    return 'Config cache cleared!';
+})->middleware('auth', 'admin');
+
+Route::get('/route-clear', function () {
+    Artisan::call('route:clear');
+    return 'Route cache cleared!';
+})->middleware('auth', 'admin');
+
+Route::get('/view-clear', function () {
+    Artisan::call('view:clear');
+    return 'View cache cleared!';
+})->middleware('auth', 'admin');
